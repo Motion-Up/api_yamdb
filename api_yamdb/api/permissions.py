9f3dd843db002a85063fdce9299f4a71d4b0a4c3
Ignat Dan <igntdan777@gmail.com>
2022-03-10 20:47:23 +0800

    fix all bugs AuthUsers

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -27,7 +27,7 @@ class IsAdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and request.user.role == 'admin'
+            and (request.user.role == 'admin' or request.user.is_superuser)
         )
 
 
